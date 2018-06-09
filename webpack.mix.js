4db15e40049b1e407886b98ac8e8115f26d26921
riv4wi <riv4wi@gmail.com>
2018-06-09 11:41:57 -0300

    Inclusión de los ccs y web fonts

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,25 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+// mix.js('resources/assets/js/app.js', 'public/js')
+//    .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.js([
+        // 'node_modules/jquery/src/jquery.js',
+        // 'resources/assets/js/bootstrap.bundle.js',
+        // 'resources/assets/js/adminlte.min.js',
+        'resources/assets/js/app.js'
+    ],
+    'public/js/app.js' )
+    .sass('resources/assets/sass/app.scss', 'public/css/app.css');
+
+// mix.js([
+//         // 'node_modules/jquery/src/jquery.js',
+//         // 'resources/assets/js/bootstrap.bundle.js',
+//         // 'resources/assets/js/adminlte.min.js',
+//         'resources/assets/js/app.js'
+//     ],
+//     'public/js/app.js' )
+//     .sass(
+//         'resources/assets/sass/app.scss',
+//     'public/css/app.css');
