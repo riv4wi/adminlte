c60cbda3c152bbeea6bd4f58db3b8fdb0b962208
riv4wi <riv4wi@gmail.com>
2018-06-10 01:21:57 -0300

    Cambiar todos los href e imagenes con asset

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,7 +11,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <title>Carma</title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <!-- Google Font: Source Sans Pro -->
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700')}}" >
     <link rel="shortcut icon" href="{{{ asset('img/favicon.ico') }}}">
 </head>
 <body class="hold-transition sidebar-mini">
@@ -56,7 +56,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                     <a href="#" class="dropdown-item">
                         <!-- Message Start -->
                         <div class="media">
-                            <img src="img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+                            <img src="{{asset('img/user1-128x128.jpg')}}" alt="User Avatar" class="img-size-50 mr-3 img-circle">
                             <div class="media-body">
                                 <h3 class="dropdown-item-title">
                                     Brad Diesel
@@ -72,7 +72,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                     <a href="#" class="dropdown-item">
                         <!-- Message Start -->
                         <div class="media">
-                            <img src="img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+                            <img src="{{asset('img/user8-128x128.jpg')}}" alt="User Avatar" class="img-size-50 img-circle mr-3">
                             <div class="media-body">
                                 <h3 class="dropdown-item-title">
                                     John Pierce
@@ -88,7 +88,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                     <a href="#" class="dropdown-item">
                         <!-- Message Start -->
                         <div class="media">
-                            <img src="img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+                            <img src="{{asset('img/user3-128x128.jpg')}}" alt="User Avatar" class="img-size-50 img-circle mr-3">
                             <div class="media-body">
                                 <h3 class="dropdown-item-title">
                                     Nora Silvester
@@ -143,10 +143,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
         <a href="index3.html" class="brand-link">
-            <img src="img/carma-logo.png" alt="Carma Logo" class="brand-image"
+            <img src="{{asset('img/carma-logo.png')}}" alt="Carma Logo" class="brand-image"
                  style="opacity: .8">
             <span class="brand-text font-weight-light">
-                 <img src="img/lema.svg" alt="Si te pesan los pies... Nuestros autos son para ti..." style="width: 8em; height: 3em;">
+                 <img src="{{asset('img/lema.svg')}}" alt="Si te pesan los pies... Nuestros autos son para ti..." style="width: 8em; height: 3em;">
             </span>
         </a>
 
@@ -155,7 +155,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <!-- Sidebar user panel (optional) -->
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
-                    <img src="img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                    <img src="{{asset('img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
                     <a href="#" class="d-block">Alexander Pierce</a>
@@ -255,7 +255,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <div class="float-right d-none d-sm-inline">
             Coded by
             <a href="https://github.com/riv4wi">
-                <img src="img/riv4w.png"  style="width: 2em; height: 2em;">
+                <img src="{{asset('img/riv4w.png')}}"  style="width: 2em; height: 2em;">
             </a>
         </div>
         <!-- Default to the left -->
